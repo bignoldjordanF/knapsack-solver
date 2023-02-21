c5afdc65cd6ca057105684c547deb972eb77650f
Fin Jordan <bignoldjordanf@gmail.com>
2023-02-21 17:52:08 +0000

    updated for namedtuple accessing

--- src/solvers/greedy.py
+++ src/solvers/greedy.py
@@ -1,20 +1,20 @@
 from knapsack import Knapsack, Allocation
 
 
-def greedy_solver(knapsack: Knapsack):
+def greedy_solver(knapsack: Knapsack) -> Allocation:
     capacity: int = knapsack.capacity
     candidates: list = [(pid, cand) for pid, cand in enumerate(knapsack.candidates)]
-    candidates = sorted(candidates, key=lambda t: t[1][1], reverse=True)
+    candidates: list = sorted(candidates, key=lambda t: t[1].value, reverse=True)
     
     allocation: list = []
     value: int = 0
     iterator: int = 0
     while 0 < capacity and iterator < len(candidates):
         cand: tuple = candidates[iterator]
-        if cand[1][0] <= capacity:
+        if cand[1].weight <= capacity:
             allocation.append(cand[0])
-            value += cand[1][1]
-            capacity -= cand[1][0]
+            value += cand[1].value
+            capacity -= cand[1].weight
         iterator += 1
     
     return Allocation(allocation, value)
