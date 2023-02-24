1309f7cd0a2919fd598b737fd26da2108736f87f
Fin Jordan <bignoldjordanf@gmail.com>
2023-02-24 00:52:45 +0000

    cleaned up branch and bound code

--- src/solvers/branch_bound.py
+++ src/solvers/branch_bound.py
@@ -1,12 +1,10 @@
 from knapsack import KnapsackInstance, KnapsackAllocation
-from collections import deque
 from dataclasses import dataclass
-
+from collections import deque
 
 # References:
 # [0] https://www.geeksforgeeks.org/implementation-of-0-1-knapsack-using-branch-and-bound/?ref=lbp
 # [1] https://stackoverflow.com/questions/43965835/knapsack-branch-and-bound-wrong-result
-
 
 @dataclass
 class KnapsackNode:
@@ -17,66 +15,68 @@ class KnapsackNode:
     allocation: list
 
 
-def bound(u, n, capacity, items):
-    if u.weight >= capacity:
+def bound(node: KnapsackNode, n_items: int, capacity: int, items: list):
+    if node.weight >= capacity:
         return 0
     
-    profit_bound: int = u.value
-    j: int = u.level + 1
-    totweight: int = u.weight
+    value_bound: int = node.value
+    level: int = node.level + 1
+    weight: int = node.weight
 
-    while j < n and totweight + items[j][1].weight <= capacity:
-        totweight += items[j][1].weight
-        profit_bound += items[j][1].value
-        j += 1
-
-    if j < n:
-        profit_bound += (capacity - totweight) * items[j][1].value / items[j][1].weight
+    while level < n_items and weight + items[level][1].weight <= capacity:
+        weight += items[level][1].weight
+        value_bound += items[level][1].value
+        level += 1
     
-    return profit_bound
+    if level < n_items:
+        value_bound += (capacity - weight) * \
+            items[level][1].value / items[level][1].weight
+    
+    return value_bound
 
 
 def branch_and_bound_solver(knapsack: KnapsackInstance) -> KnapsackAllocation:
-    candidates: list = [(pid, cand) for pid, cand in enumerate(knapsack.items)]
-    items: list = sorted(candidates, key=lambda t: t[1].value/t[1].weight, reverse=True)
-    total_items = len(candidates)
-    weight = knapsack.capacity
+    items: list = [(pid, cand) for pid, cand in enumerate(knapsack.items)]
+    items: list = sorted(items, key=lambda i: i[1].value/i[1].weight, reverse=True)
 
-    Q: deque = deque()
-    u: KnapsackNode = KnapsackNode(-1, 0, 0, 0, [])
-    Q.append(u)
+    queue: deque = deque()
+    queue.append(KnapsackNode(-1, 0, 0, 0, []))
 
-    maxProfit: int = 0
-    maxAllocation: list = []
-    while Q:
-        u = Q.popleft()
-        v = KnapsackNode(0, 0, 0, 0, [])
-        if u.level == -1:
-            v.level = 0
+    max_value: int = 0
+    max_allocation: list = []
 
-        if u.level == total_items - 1:
+    while queue:
+        curr: KnapsackNode = queue.popleft()
+        child: KnapsackNode = KnapsackNode(0, 0, 0, 0, [])
+
+        if curr.level == -1:
+            child.level = 0
+        
+        if curr.level == len(items) - 1:
             continue
 
-        v.level = u.level + 1
-        v.weight = u.weight + items[v.level][1].weight
-        v.value = u.value + items[v.level][1].value
-        v.allocation = u.allocation[:] + [items[v.level][0]]
+        child.level = curr.level + 1
+        child.weight = curr.weight + items[child.level][1].weight
+        child.value = curr.value + items[child.level][1].value
+        child.allocation = curr.allocation[:] + [items[child.level][0]]
+        child.bound = bound(child, len(items), knapsack.capacity, items)
 
-        if (v.weight <= weight and v.value > maxProfit):
-            maxProfit = v.value
-            maxAllocation = v.allocation
-
-        v.bound = bound(v, total_items, weight, items)
-        if (v.bound > maxProfit):
-            Q.append(v)
-
-        v = KnapsackNode(0, 0, 0, 0, []) 
-        v.level = u.level + 1   
-        v.weight = u.weight
-        v.value = u.value
-        v.allocation = u.allocation[:]
-        v.bound = bound(v, total_items, weight, items)
-        if (v.bound > maxProfit):
-            Q.append(v)
+        if child.weight <= knapsack.capacity and child.value > max_value:
+            max_value = child.value
+            max_allocation = child.allocation
         
-    return KnapsackAllocation(maxAllocation, maxProfit)
+        if child.bound > max_value:
+            queue.append(child)
+
+        child = KnapsackNode(0, 0, 0, 0, [])
+        child.level = curr.level + 1
+        child.weight = curr.weight
+        child.value = curr.value
+        child.allocation = curr.allocation[:]
+        child.bound = bound(child, len(items), knapsack.capacity, items)
+
+        if child.bound > max_value:
+            queue.append(child)
+    
+    return KnapsackAllocation(max_allocation, max_value)
+
