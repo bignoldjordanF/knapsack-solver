ee0676d6869c16fee602508106d1a27dbff1907b
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-01 20:57:45 +0000

    solver algorithms can be accessed directly by importing solver

---
+++ src/solvers/__init__.py
@@ -0,0 +1,5 @@
+from .greedy import greedy_solver, ratio_greedy_solver
+from .dyn_prog import dynamic_programming_solver
+from .branch_bound import branch_and_bound_solver
+from .sim_anneal import simulated_annealing_solver
+from .genetic import genetic_algorithm_solver
