3b3988be0cbec140636702a7d36f82a31413fba1
Fin Jordan <bignoldjordanf@gmail.com>
2023-03-01 18:10:13 +0000

    example now runs genetic algorithm

--- src/example.py
+++ src/example.py
@@ -3,15 +3,16 @@ from solvers.greedy import greedy_solver, ratio_greedy_solver
 from solvers.dyn_prog import dynamic_programming_solver
 from solvers.branch_bound import branch_and_bound_solver
 from solvers.sim_anneal import simulated_annealing_solver
+from solvers.genetic import genetic_algorithm_solver
 
 
 generator: KnapsackGenerator = KnapsackGenerator(
-    min_capacity=500000,
+    min_capacity=50000,
     max_capacity=1000000,
     min_num_items=5,
-    max_num_items=25,
+    max_num_items=100,
     min_weight=1000,
-    max_weight=500000,
+    max_weight=40000,
     min_value=1,
     max_value=10
 )
@@ -20,7 +21,7 @@ knapsack: KnapsackInstance = generator.generate()
 print()
 print('KNAPSACK INSTANCE')
 print('-' * 17)
-print(knapsack)
+print(knapsack.con())
 
 print()
 print('LINEAR APPROXIMATIONS')
@@ -32,6 +33,7 @@ print()
 print('META-HEURISTICS')
 print('-' * 15)
 print(f'Simulated Annealing: {simulated_annealing_solver(knapsack)}')
+print(f'Genetic Algorithm: {genetic_algorithm_solver(knapsack)}')
 
 print()
 print('EXACT ALGORITHMS')
